d195b54b8ffa06b70f0c8814094e17da3940f8cf
Aleksei Bezdetko <ggalliosha@gmail.com>
2020-07-19 18:06:11 +0300

    Create project frontend base with react, ts and apollo-client

---
+++ client/src/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import * as serviceWorker from "./serviceWorker";
+import { Spells } from "./pages";
+
+const client = new ApolloClient({
+  uri: "/api",
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Spells />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
