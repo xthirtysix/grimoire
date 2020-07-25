2eddb78acb5280b8601bf28ff03fe29be9062b0f
Aleksei Bezdetko <ggalliosha@gmail.com>
2020-07-25 22:39:49 +0300

    Add user page data and interface base

--- client/src/lib/graphql/queries/index.ts
+++ client/src/lib/graphql/queries/index.ts
@@ -1,1 +1,3 @@
 export * from "./AuthUrl";
+export * from "./User";
+ 
--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 import { MongoClient } from "mongodb";
-import { Spell, User, Database } from "../lib/types";
+import { Spell, User, Grimoire, Database } from "../lib/types";
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
@@ -15,6 +15,7 @@ export const connectDatabase = async (): Promise<Database> => {
 
   return {
     spells: db.collection<Spell>("spells"),
-    users: db.collection<User>("users")
+    users: db.collection<User>("users"),
+    grimoires: db.collection<Grimoire>("grimoires"),
   };
 };
