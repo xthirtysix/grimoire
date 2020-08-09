288912c91289dd49070c9921442be9adf770abe1
Aleksei Bezdetko <ggalliosha@gmail.com>
2020-08-09 22:46:46 +0300

    Fix Sidebar styling

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -1,8 +1,7 @@
-import React, { useState, ReactNode } from "react";
-import { Typography, List, Drawer, Hidden, Divider } from "@material-ui/core";
+import React, { ReactNode } from "react";
+import { Typography, List, Drawer, Toolbar, Divider } from "@material-ui/core";
 import {
   makeStyles,
-  useTheme,
   Theme,
   createStyles,
 } from "@material-ui/core/styles";
@@ -12,17 +11,14 @@ const drawerWidth = 240;
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     drawer: {
-      [theme.breakpoints.up("sm")]: {
-        width: drawerWidth,
-        flexShrink: 0,
-      },
+      width: drawerWidth,
+      flexShrink: 0,
     },
-    // necessary for content to be below app bar
-    toolbar: { ...theme.mixins.toolbar, padding: "2px 16px" },
-    header: { marginTop: "1rem" },
-    subheader: { color: "grey", fontSize: "0.8rem" },
     drawerPaper: {
       width: drawerWidth,
+    },
+    drawerContainer: {
+      overflow: "auto",
     },
     content: {
       flexGrow: 1,
@@ -39,60 +35,20 @@ interface Props {
 }
 
 export const Sidebar = ({ drawerItems, window, header, subHeader }: Props) => {
-  const [mobileOpen, setMobileOpen] = useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
   const classes = useStyles();
-  const theme = useTheme();
-
-  const drawer = (
-    <div>
-      <div className={classes.toolbar}>
-        <Typography className={classes.header}>{header}</Typography>
-        <Typography className={classes.subheader}>{subHeader}</Typography>
-      </div>
-      <Divider />
-      <List>{drawerItems}</List>
-    </div>
-  );
-
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <nav className={classes.drawer} aria-label="mailbox folders">
-      {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-      <Hidden smUp implementation="css">
-        <Drawer
-          container={container}
-          variant="temporary"
-          anchor={theme.direction === "rtl" ? "right" : "left"}
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </Hidden>
-      <Hidden xsDown implementation="css">
-        <Drawer
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-          variant="permanent"
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Hidden>
-    </nav>
+    <Drawer
+      className={classes.drawer}
+      variant="permanent"
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+    >
+      <Toolbar />
+      <div className={classes.drawerContainer}>
+        <List>{drawerItems}</List>
+      </div>
+    </Drawer>
   );
 };
--- client/src/components/SidebarSkeleton/index.tsx
+++ client/src/components/SidebarSkeleton/index.tsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { Drawer, Hidden, Divider } from "@material-ui/core";
+import { Drawer, Toolbar } from "@material-ui/core";
 import { Skeleton } from "@material-ui/lab";
-import {
-  makeStyles,
-  useTheme,
-  Theme,
-  createStyles,
-} from "@material-ui/core/styles";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 
 const drawerWidth = 240;
 
@@ -22,7 +17,7 @@ const useStyles = makeStyles((theme: Theme) =>
     toolbar: { ...theme.mixins.toolbar, padding: "2px 16px" },
     header: { marginTop: "1.2rem" },
     list: { padding: "24px 16px 0" },
-    listItem: { margin: "1.5rem 0" },
+    listItem: { margin: "1.4 rem 0" },
     drawerPaper: {
       width: drawerWidth,
     },
@@ -33,72 +28,69 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-interface Props {
-  window?: () => Window;
-}
-
-export const SidebarSkeleton = ({ window }: Props) => {
+export const SidebarSkeleton = () => {
   const classes = useStyles();
-  const theme = useTheme();
-
-  const drawer = (
-    <div>
-      <div className={classes.toolbar}>
-        <Skeleton
-          className={classes.header}
-          variant="text"
-          width={110}
-          height={19}
-        />
-        <Skeleton variant="text" width={140} height={16} />
-      </div>
-      <Divider />
-      <div className={classes.list}>
-        <Skeleton variant="text" width={80} height={16}></Skeleton>
-        <Skeleton className={classes.listItem} variant="text" width={110} height={20}></Skeleton>
-        <Skeleton className={classes.listItem} variant="text" width={80} height={20}></Skeleton>
-        <Skeleton className={classes.listItem} variant="text" width={70} height={20}></Skeleton>
-        <Skeleton className={classes.listItem} variant="text" width={90} height={20}></Skeleton>
-        <Skeleton className={classes.listItem} variant="text" width={95} height={20}></Skeleton>
-        <Skeleton className={classes.listItem} variant="text" width={70} height={20}></Skeleton>
-        <Skeleton className={classes.listItem} variant="text" width={100} height={20}></Skeleton>
-        <Skeleton className={classes.listItem} variant="text" width={90} height={20}></Skeleton>
-      </div>
-    </div>
-  );
-
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <nav className={classes.drawer} aria-label="mailbox folders">
-      {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-      <Hidden smUp implementation="css">
-        <Drawer
-          container={container}
-          variant="temporary"
-          anchor={theme.direction === "rtl" ? "right" : "left"}
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </Hidden>
-      <Hidden xsDown implementation="css">
-        <Drawer
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-          variant="permanent"
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Hidden>
-    </nav>
+    <Drawer
+      className={classes.drawer}
+      variant="permanent"
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+    >
+      <Toolbar />
+      <div className={classes.list}>
+        <Skeleton variant="text" width={80} height={16}></Skeleton>
+        <Skeleton
+          className={classes.listItem}
+          variant="text"
+          width={110}
+          height={20}
+        ></Skeleton>
+        <Skeleton
+          className={classes.listItem}
+          variant="text"
+          width={80}
+          height={20}
+        ></Skeleton>
+        <Skeleton
+          className={classes.listItem}
+          variant="text"
+          width={70}
+          height={20}
+        ></Skeleton>
+        <Skeleton
+          className={classes.listItem}
+          variant="text"
+          width={90}
+          height={20}
+        ></Skeleton>
+        <Skeleton
+          className={classes.listItem}
+          variant="text"
+          width={95}
+          height={20}
+        ></Skeleton>
+        <Skeleton
+          className={classes.listItem}
+          variant="text"
+          width={70}
+          height={20}
+        ></Skeleton>
+        <Skeleton
+          className={classes.listItem}
+          variant="text"
+          width={100}
+          height={20}
+        ></Skeleton>
+        <Skeleton
+          className={classes.listItem}
+          variant="text"
+          width={90}
+          height={20}
+        ></Skeleton>
+      </div>
+    </Drawer>
   );
 };
