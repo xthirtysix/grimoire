36e738d65f8b165a2a299fc46f6841bfc42826f3
Aleksei Bezdetko <ggalliosha@gmail.com>
2020-09-08 10:05:12 +0300

    Rewrite Spell resolver

--- server/src/lib/utils/index.ts
+++ server/src/lib/utils/index.ts
@@ -1,5 +1,7 @@
 import { Request } from "express";
+import { ObjectId } from "mongodb";
 import { Database, User } from "../types";
+import { SpellsFilter } from "../../graphql/resolvers/Spell/types";
 
 export const authorize = async (
   db: Database,
@@ -12,4 +14,28 @@ export const authorize = async (
   });
 
   return viewer;
+};
+
+export const createFilterQuery = (
+  filterValue: string[],
+  grimoireSpells?: ObjectId[]
+) => {
+  let query: any = { $match: {} };
+
+  if (grimoireSpells) {
+    query["$match"]["_id"] = { $in: grimoireSpells };
+    query["$match"]["school"] = { $in: filterValue };
+
+    return query;
+  } else {
+    query["$match"]["school"] = { $in: filterValue };
+  }
+
+  return query;
+};
+
+export const transformFilterValues = (filters: SpellsFilter[]) => {
+  return filters.map((value) => {
+    return value.charAt(0) + value.slice(1).toLowerCase();
+  });
 };
